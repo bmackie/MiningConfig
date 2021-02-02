6b6e5eab75a64167a6a2c9a4e26ff7b1bcf4fa74
bmackie <the.bmackie@gmail.com>
2021-02-02 13:59:24 -0600

    changed monitorCheck path

--- monitorCheck/monitorCheck.sh
+++ monitorCheck/monitorCheck.sh
@@ -5,10 +5,10 @@ PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 if pgrep -x python > /dev/null
 then
         echo "Monitor is running"
-	date +"%D %T Monitor is running" >> /home/ethos/monitorLog.log
+	date +"%D %T Monitor is running" >> /home/ethos/monitorCheck/monitorLog.log
 else
 	echo "Monitor found not running"
 	echo "launching Monitor"
-	date +"%D %T No monitor found running Launching monitor" >> /home/ethos/monitorLog.log
-        python /home/ethos/ethos_monitor/monitor.py c44e8e "http://markcp.ethosdistro.com/?json=yes"
+	date +"%D %T No monitor found running Launching monitor" >> /home/ethos/monitorCheck/monitorLog.log
+        python /home/ethos/monitorCheck/monitor.py c44e8e "http://markcp.ethosdistro.com/?json=yes"
 fi
